7d514a63052aede828b9e008ac07db2c9377cfaa
kimsolee95 <ahhasolee@naver.com>
2023-08-27 01:16:23 +0900

    test: (JPA repository test) member entity 생성 확인

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,26 +1,55 @@
 package study.datajpa.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
   @Id @GeneratedValue
+  @Column(name = "member_id")
   private Long id;
   private String username;
+  private int age;
 
-  protected Member() {
-  }
+  @ManyToOne
+  @JoinColumn(name = "team_id")
+  private Team team;
+
+//  protected Member() {
+//  }
 
   public Member(String username) {
     this.username = username;
   }
 
+  public Member(String username, int age, Team team) {
+
+    this.username = username;
+    this.age = age;
+    if (team != null) {
+      changeTeam(team);
+    }
+  }
+
   public void changeUsername(String username) {
     this.username = username;
   }
+
+  public void changeTeam(Team team) {
+    this.team = team;
+    team.getMembers().add(this);
+  }
+
 }
