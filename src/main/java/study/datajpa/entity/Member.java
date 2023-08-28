9c6a1e48cff06d89a1e36dea51bf03f74ef4ee5d
kimsolee95 <ahhasolee@naver.com>
2023-08-29 00:02:33 +0900

    test: (JPA repository test) member entity 에 대한 CRUD test

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -2,6 +2,7 @@ package study.datajpa.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -23,7 +24,7 @@ public class Member {
   private String username;
   private int age;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "team_id")
   private Team team;
 
