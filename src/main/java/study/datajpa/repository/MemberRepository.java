f09f50727fba53608abe1abae500ccbe480392e5
kimsolee95 <ahhasolee@naver.com>
2023-09-02 00:06:56 +0900

    test: (JPA repository test) namedQuery 동작 test

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -2,9 +2,13 @@ package study.datajpa.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-  List<Member> findByUsernameAndAgeGreaterThen(String username, int age);
+  List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+//  @Query(name = "Member.findByUsername")
+  List<Member> findByUsername(@Param("username")String username);
 }
