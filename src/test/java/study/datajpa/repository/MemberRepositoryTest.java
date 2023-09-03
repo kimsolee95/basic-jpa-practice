87a8a2f895f2fa3b681883a41ee354df489e2aa7
kimsolee95 <ahhasolee@naver.com>
2023-09-03 09:06:29 +0900

    test: (JPA repository test) repository를 통해 만든 메서드의 반환타입 확인

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -2,13 +2,16 @@ package study.datajpa.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 @SpringBootTest
 @Transactional
@@ -17,6 +20,9 @@ public class MemberRepositoryTest {
 
   @Autowired
   MemberRepository memberRepository;
+
+  @Autowired
+  TeamRepository teamRepository;
 
   @Test
   public void testMember() {
@@ -82,6 +88,68 @@ public class MemberRepositoryTest {
     List<Member> result = memberRepository.findByUsername("AAA");
     Member findMember = result.get(0);
     assertThat(findMember).isEqualTo(m1);
+  }
+
+  @Test
+  public void testQuery() {
+    Member m1 = new Member("AAA", 10);
+    Member m2 = new Member("BBB", 20);
+    memberRepository.save(m1);
+    memberRepository.save(m2);
+
+    List<Member> result = memberRepository.findUser("AAA", 10);
+    assertThat(result.get(0)).isEqualTo(m1);
+  }
+
+  @Test
+  public void findUsernameList() {
+    Member m1 = new Member("AAA", 10);
+    Member m2 = new Member("BBB", 20);
+    memberRepository.save(m1);
+    memberRepository.save(m2);
+
+    List<String> usernameList = memberRepository.findUsernameList();
+    for (String s : usernameList) {
+      System.out.println("S = " + s);
+    }
+  }
+
+  @Test
+  public void findMemberDto() {
+    Team team = new Team("teamA");
+    teamRepository.save(team);
+
+    Member m1 = new Member("AAA", 10);
+    m1.setTeam(team);
+    memberRepository.save(m1);
+
+    List<MemberDto> memberDtos = memberRepository.findMemberDto();
+    for (MemberDto dto : memberDtos) {
+      System.out.println("dto = " + dto);
+    }
+  }
+
+  @Test
+  public void findByNames() {
+    Member m1 = new Member("AAA", 10);
+    Member m2 = new Member("BBB", 20);
+    memberRepository.save(m1);
+    memberRepository.save(m2);
+
+    List<Member> usernameList = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+    for (Member member : usernameList) {
+      System.out.println("member = " + member);
+    }
+  }
+
+  @Test
+  public void returnType() {
+    Member m1 = new Member("AAA", 10);
+    Member m2 = new Member("BBB", 20);
+    memberRepository.save(m1);
+    memberRepository.save(m2);
+
+    List<Member> aaa = memberRepository.findListByUsername("AAA");
   }
 
 }
