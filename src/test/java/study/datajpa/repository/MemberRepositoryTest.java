5e0143ce56223ad842ad08de357a55300e363259
kimsolee95 <ahhasolee@naver.com>
2023-09-12 22:13:08 +0900

    test: (JPA repository test) custom repository 활용

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -253,4 +253,9 @@ public class MemberRepositoryTest {
     List<Member> result = memberRepository.findLockByUsername("member1");
   }
 
+  @Test
+  public void callCustom() {
+    List<Member> result = memberRepository.findMemberCustom();
+  }
+
 }
