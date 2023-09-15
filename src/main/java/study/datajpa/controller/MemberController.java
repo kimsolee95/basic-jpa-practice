9f56eed3fcfd56bf21343bf7e60b2b16def8411d
kimsolee95 <ahhasolee@naver.com>
2023-09-15 23:21:20 +0900

    feat: page 객체 사용한 list 반환

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -2,6 +2,8 @@ package study.datajpa.controller;
 
 import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,8 +27,17 @@ public class MemberController {
     return member.getUsername();
   }
 
+  @GetMapping("/members")
+  public Page<Member> list(Pageable pageable) {
+    return memberRepository.findAll(pageable);
+  }
+
   @PostConstruct
   public void init() {
-    memberRepository.save(new Member("userA"));
+    for (int i=0; i<100; i++) {
+      memberRepository.save(new Member("user" + i, i));
+    }
   }
+
 }
+
